808b18bfd0459b2763133f852b1e6f8c700ad88d
Guixter <g.singland@gmail.com>
2020-02-15 14:59:28 -0500

    Adding operators

--- Class3/Types/Types.py
+++ Class3/Types/Types.py
@@ -1,6 +1,7 @@
 ######################################
 #            TYPES
 ######################################
+from enum import Enum;
 
 # Création d'une variable int
 monInt = 35;
@@ -22,9 +23,20 @@ print(type(monBool), monBool, '\n');
 monString = "Hello";
 print(type(monString), monString, '\n');
 
+
+
 #### TODO (CASTING) :
 # - Afficher le type de la variable ageUtilisateur
 # - Créer une variable realAgeUtilisateur qui cast la variable ageUtilisateur en int.
 # - Afficher le type et la valeur de la variable realAgeUtilisateur.
 
 ageUtilisateur = input("Quel est votre âge ?");
+
+
+
+
+#### TODO (CUSTOM TYPES) :
+# - Définir son propre enum (ça peut être ce que vous voulez, soyez inventifs !)
+# - Créer une variable utilisant une des valeurs de votre enum
+# - Afficher le type et la valeur de cette variable
+
