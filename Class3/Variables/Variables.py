f43e65373dbbf6bfb383a5703dfd3cb7e38067f8
Guixter <g.singland@gmail.com>
2020-02-15 14:15:04 -0500

    Adding the variables project

---
+++ Class3/Variables/Variables.py
@@ -0,0 +1,24 @@
+######################################
+#            VARIABLES
+######################################
+
+print ("Début du test.", '\n');
+
+# Création d'une variable à partir d'une constante
+maVariable = 35;
+print(maVariable, '\n');
+
+# Création d'une copie de maVariable
+maCopie = maVariable;
+print(maCopie, '\n');
+
+# Modification de maVariable
+maVariable = 45;
+print(maVariable);
+print(maCopie, '\n');
+
+#### TODO :
+# - Créer une nouvelle variable monAutreCopie, qui sera une copie de maVariable
+# - Attributer la valeur de monAutreCopie dans la variable maCopie
+# - Afficher la valeur des 3 variables
+
