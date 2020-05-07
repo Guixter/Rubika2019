2d466102a64ebded54f7b6cfc4ad1ef7ba960f10
gsingland <guillaume.singland@illogika.com>
2020-05-07 15:28:48 +0200

    Adding class6

---
+++ Class6/Fonctions/Fonctions.py
@@ -0,0 +1,30 @@
+######################################
+#             FONCTIONS
+######################################
+
+def MaFonction1():
+	print("Hello World");
+
+MaFonction1();
+MaFonction1();
+
+#### TODO
+# Créer une fonction qui prend un nom en paramètre, et qui affiche le texte "bonjour [NOM]".
+# Puis appeler cette fonction plusieurs fois avec plusieurs noms différents.
+# Bonus : on peut aussi appeller cette fonction avec le résultat d'un appel à input().
+
+
+# Créer une fonction qui prend 2 nombres en paramètre, et qui retourne la somme des 2 nombres.
+# Puis appeler cette fonction plusieurs fois avec plusieurs entrées différentes, et afficher le résultat.
+
+
+#### TODO
+# Comment écrire une fonction qui ajoute 1 à la variable qu'on fournit en entrée ?
+
+
+#### TODO
+# Écrire une fonction récursive permettant d'écrire 5 fois "Bonjour".
+
+
+#### TODO
+# Comment écrire une fonction qui ajoute 1 à la variable qu'on fournit en entrée ?
---
+++ Class6/Librairies/Librairies.py
@@ -0,0 +1,8 @@
+######################################
+#             LIBRAIRIES
+######################################
+
+#### TODO
+# Créer votre propre librairie, dans laquelle vous définirez une fonction qui affiche "Bonjour".
+# Puis, dans le fichier courant, importer cette nouvelle librairie, et appeler la fonction.
+
